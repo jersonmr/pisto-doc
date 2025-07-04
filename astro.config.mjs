5dc4529c0693eb139f6899bb67d6d3111456d528
jersonmr <jersonmr@gmail.com>
2025-07-04 12:57:15 -0400

    Refactor Astro configuration by removing unused integrations, updating dependencies, and enhancing login functionality with Firebase authentication. Introduce SweetAlert2 for error handling in the login process and create a new version documentation page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-import node from "@astrojs/node";
-import clerk from "@clerk/astro";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    output: "server",
-    adapter: node({ mode: "standalone" }),
-
     integrations: [
-        clerk(),
         starlight({
             title: "Pisto Docs",
-            social: [
-                {
-                    icon: "github",
-                    label: "GitHub",
-                    href: "https://github.com/withastro/starlight",
-                },
-            ],
-            // Configuración de overrides para componentes personalizados
-            components: {
-                Header: "./src/components/Header.astro",
-            },
             sidebar: [
                 {
                     label: "Guides",
@@ -41,8 +23,8 @@ export default defineConfig({
             ],
         }),
     ],
-
     vite: {
+        // @ts-ignore
         plugins: [tailwindcss()],
     },
 });
--- src/actions/login.action.ts
+++ src/actions/login.action.ts
@@ -1,5 +1,9 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { firebase } from "@/firebase/config";
+import { FirebaseError } from "firebase/app";
+// import { redirect } from "astro:router";
 // import { zfd } from "zod-form-data";
 
 export const loginAction = defineAction({
@@ -21,6 +25,35 @@ export const loginAction = defineAction({
             cookies.delete("email", {
                 path: "/",
             });
+        }
+
+        try {
+            const userCredentials = await signInWithEmailAndPassword(
+                firebase.auth,
+                email,
+                password
+            );
+            const user = userCredentials.user;
+            console.log(user);
+
+            // if (user) {
+            //     redirect("/");
+            // }
+        } catch (error) {
+            console.error(error);
+            const firebaseError = error as FirebaseError;
+
+            if (firebaseError.code === "auth/invalid-credential") {
+                throw new Error("Credenciales inválidas");
+            } else if (firebaseError.code === "auth/invalid-email") {
+                throw new Error("Email inválido");
+            } else if (firebaseError.code === "auth/user-not-found") {
+                throw new Error("Usuario no encontrado");
+            } else if (firebaseError.code === "auth/wrong-password") {
+                throw new Error("Contraseña incorrecta");
+            } else {
+                throw new Error("Error al iniciar sesión");
+            }
         }
 
         return {
