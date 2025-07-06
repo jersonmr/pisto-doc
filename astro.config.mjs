33335cdd195a27d31dd1d0762151246b0b1015e6
jersonmr <jersonmr@gmail.com>
2025-07-06 11:28:58 -0400

    Remove trailing slash enforcement from Astro configuration to streamline URL handling.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
     site: "https://docs.pisto.co",
-    trailingSlash: "always",
     integrations: [
         starlight({
             title: "Pisto Docs",
